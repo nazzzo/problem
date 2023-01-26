46d67ee8fc2ec50d1025454c831b94efd6f51927
kimjh <hecticika@gmail.com>
2023-01-26 09:19:51 +0900

    fix: 진짜진짜 수정

--- 김주형/practice6/front/public/js/module/join.js
+++ 김주형/practice6/front/public/js/module/join.js
@@ -1,6 +1,6 @@
 import request from "/js/lib/request.js";
 
-const checkInput = async (inputId, reg, callback) => {
+const checkInput = async (inputId, reg) => {
   document
     .querySelector(`#${inputId}Box`)
     .addEventListener("keyup", async () => {
@@ -59,7 +59,7 @@ const config = {
 };
 
 for (const key in config) {
-  checkInput(key, config[key].reg, config[key].callback);
+  checkInput(key, config[key].reg);
 }
 
 // 패스워드 재확인
